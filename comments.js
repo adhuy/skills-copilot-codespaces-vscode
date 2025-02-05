b2638f35e75310e28769b3a3a89234c4d187d053
yudhamp <yudhamerdeka91@gmail.com>
2025-02-05 09:51:51 +0000

    bugfix comments.js

--- comments.js
+++ comments.js
@@ -1,7 +1,7 @@
 // create web server
-const express = require('express');
+/* const express = require('express');
 const app = express();
 app.use(express.static('static_files'));
 app.listen(3000, function() {
     console.log('Server is running on http://localhost:3000');
-});
+}); */
