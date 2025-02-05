8291646eeb16e6b8b74a43c6bc7301816669594b
yudhamp <yudhamerdeka91@gmail.com>
2025-02-05 09:46:43 +0000

    bugfix comments.js

--- comments.js
+++ comments.js
@@ -1,13 +1,43 @@
 // create web server
-var express = require('express');
-var app = express();
+// node comments.js
+// open browser and go to http://localhost:8080
+// type in comment and hit submit
 
-// create a route
-app.get('/comments', function(req, res){
-	res.send('This is the comments page');
+// require express
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+var fs = require('fs');
+var comments = [];
+
+// set up static file server
+app.use(express.static('public'));
+
+// set up a route for the root of the site
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// start server
-app.listen(3000, function(){
-	console.log('Server is running on http://localhost:3000');
+// set up a route for posting comments
+app.post('/comment', function(req, res) {
+  var comment = req.body.comment;
+  console.log('comment:', comment);
+  comments.push(comment);
+  fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
+    if (err) {
+      console.log('error writing comments to file');
+    }
+  });
+  res.redirect('/');
+});
+
+// set up a route for getting all comments
+app.get('/comments', function(req, res) {
+  res.send(JSON.stringify(comments));
+});
+
+// start the server
+app.listen(8080, function() {
+  console.log('Server running at http://localhost:8080');
 });
